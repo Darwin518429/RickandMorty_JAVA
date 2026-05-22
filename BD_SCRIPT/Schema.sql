7ebd4f3dc0f68cbd02e3aaf15689b48911480469
Darwin <d.biag@sapalomera.cat>
2026-05-22 23:24:26 +0200

    Arxius

--- BD_SCRIPT/Schema.sql
+++ BD_SCRIPT/Schema.sql
@@ -9,13 +9,6 @@ CREATE TABLE localitzacions (
     CONSTRAINT pk_localitzacions PRIMARY KEY(id_localitzacions)
 );
 
-CREATE TABLE episodis (
-    id_episodis INT,
-    name        VARCHAR(45)  NOT NULL,
-    data        DATE,
-    episodi     VARCHAR(45),
-    CONSTRAINT pk_episodis PRIMARY KEY(id_episodis)
-);
 
 CREATE TABLE personatges (
     id_personatge INT          PRIMARY KEY,
@@ -28,13 +21,4 @@ CREATE TABLE personatges (
     localitzacio  INT 
   ,CONSTRAINT fk_personatges_localitzacions_origen  FOREIGN KEY (origen)       REFERENCES localitzacions(id_localitzacions),
    CONSTRAINT fk_persontages_localitzacions_localitzacio FOREIGN KEY (localitzacio) REFERENCES localitzacions(id_localitzacions)
-);
--- TRUNCATE TABLE personatges_episodis;
-
-CREATE TABLE personatges_episodis (
-    personatges_id_personatge INT NOT NULL,
-    episodis_id_episodis      INT NOT NULL,
-	CONSTRAINT  personatges_episodis PRIMARY KEY (personatges_id_personatge, episodis_id_episodis),
-	CONSTRAINT	fk_personatges_personatges_episodis  FOREIGN KEY (personatges_id_personatge) REFERENCES personatges(id_personatge) ON DELETE CASCADE ON UPDATE CASCADE ,
-    CONSTRAINT 	fk_episodis_personatges_episodis FOREIGN KEY (episodis_id_episodis) REFERENCES episodis(id_episodis) ON DELETE CASCADE ON UPDATE CASCADE 
 );
--- BD_SCRIPT/data.sql
+++ BD_SCRIPT/data.sql
@@ -1,5 +1,6 @@
 USE rickandmorty;
 -- LOCALITZACIONS
+INSERT IGNORE INTO localitzacions VALUES (5000, 'DESCONEGUT',null);
 INSERT IGNORE INTO localitzacions(id_localitzacions , nom) VALUES (0, 'DESCONEGUT');
 INSERT IGNORE INTO localitzacions VALUES (1, 'EDFth (C-137)', 'Planet');
 INSERT IGNORE INTO localitzacions VALUES (2, 'AFDSdango', 'Cluster');
@@ -9,12 +10,6 @@ INSERT IGNORE INTO localitzacions VALUES (5, 'Anatomy Park', 'Microverse');
 INSERT IGNORE INTO localitzacions VALUES (20, 'Earth (Replacement Dimension)', 'Planet');
 INSERT IGNORE INTO localitzacions VALUES (21, 'Testicle Monster Dimension', 'Dimension');
 
--- EPISODIS
-INSERT IGNORE INTO episodis VALUES (1, 'Pilot', '2013-12-02', 'S01E01');
-INSERT IGNORE INTO episodis VALUES (2, 'Lawnmower DDg', '2013-12-09', 'S01E02');
-INSERT IGNORE INTO episodis VALUES (3, 'Anatomy Park', '2013-12-16', 'S01E03');
-INSERT IGNORE INTO episodis VALUES (4, 'M. Night Shaym-Aliens!', '2014-01-13', 'S01E04');
-INSERT IGNORE INTO episodis VALUES (5, 'Meeseeks and Destroy', '2014-01-20', 'S01E05');
 
 -- PERSONATGES
 INSERT IGNORE INTO personatges VALUES (1, 'Rick Sanchez', 'Alive', 'Human', '', 'Male', 1, 3);
@@ -25,17 +20,3 @@ INSERT IGNORE INTO personatges VALUES (5, 'Jerry Smith', 'Alive', 'Human', '', '
 INSERT IGNORE INTO personatges VALUES (6, 'Abadango Cluster Princess', 'Alive', 'Alien', '', 'Female', 2, 2);
 INSERT IGNORE INTO personatges VALUES (7, 'Abradolf Lincler', 'unknown', 'Human', 'Genetic experiment', 'Male', 20, 21);
 INSERT IGNORE INTO personatges VALUES (8, 'Adjudicator Rick', 'Dead', 'Human', '', 'Male', 3, 3);
-
--- PERSONATGES_EPISODIS
-INSERT IGNORE INTO personatges_episodis VALUES (1, 1);
-INSERT IGNORE INTO personatges_episodis VALUES (1, 2);
-INSERT IGNORE INTO personatges_episodis VALUES (1, 3);
-INSERT IGNORE INTO personatges_episodis VALUES (2, 1);
-INSERT IGNORE INTO personatges_episodis VALUES (2, 2);
-INSERT IGNORE INTO personatges_episodis VALUES (2, 3);
-INSERT IGNORE INTO personatges_episodis VALUES (3, 3);
-INSERT IGNORE INTO personatges_episodis VALUES (4, 3);
-INSERT IGNORE INTO personatges_episodis VALUES (5, 3);
-INSERT IGNORE INTO personatges_episodis VALUES (6, 1);
-INSERT IGNORE INTO personatges_episodis VALUES (7, 2);
-INSERT IGNORE INTO personatges_episodis VALUES (8, 4);
